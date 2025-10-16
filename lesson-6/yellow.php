cf526e1c2e30ca58ee921709da37cbc4184a478f
Callum Wand <cwrwand@gmail.com>
2025-10-16 22:01:30 +0000

    Fixed global and footer navigation across site

--- lesson-6/yellow.php
+++ lesson-6/yellow.php
@@ -14,7 +14,14 @@
 			<h1>HTML, CSS, and JS Fundamentals</h1>
 			<h2>Building Blocks for Web Developers</h2>
 			<!-- Global site navigation -->
-
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="red.php">Red blocks</a></li>
+					<li><a href="blue.php">Blue blocks</a></li>
+					<li><a href="yellow.php">Yellow blocks</a></li>
+				</ul>
+			</nav>
 		</header>
 		<!-- Page-level main content -->
 		<main>
